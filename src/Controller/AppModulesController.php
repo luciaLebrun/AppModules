e276c0990d87057138364df5380ac78f1ae8fcb7
Sophie Pan <pan.sophie.sco@gmail.com>
2021-01-21 00:40:24 +0100

    #2 Ajout des Domains UE12 et UE22

--- src/Controller/AppModulesController.php
+++ src/Controller/AppModulesController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Semaine;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,12 +13,20 @@ class AppModulesController extends AbstractController
     /**
      * @Route("/", name="maquette")
      */
-    public function index(): Response
+    public function index(): RedirectResponse
+    {
+        return $this->redirectToRoute('/Semestre1');
+    }
+
+    /**
+     * @Route("/Semestre1", name="maquette")
+     */
+    public function Semestre1(): Response
     {
         $semainerepo=$this->getDoctrine()->getRepository(Semaine::class);
 
         $semainesUE11=$semainerepo->toutesLesSemainesDeLUE11();
-        $semainesUE21=$semainerepo->toutesLesSemainesDeLUE21();
+        $semainesUE12=$semainerepo->toutesLesSemainesDeLUE12();
 
         $ddatedebscol = "2019-09-02";
         $datedebscol = new \DateTime($ddatedebscol);
@@ -29,6 +38,7 @@ class AppModulesController extends AbstractController
 
         return $this->render('AppModules/MaquetteEnseignement.html.twig', [
             'semainesUE11'=>$semainesUE11,
+            'semainesUE12'=>$semainesUE12,
             'semainedebscol'=>$semainedebscol,
             'semainefinscol'=>$semainefinscol,
         ]);
