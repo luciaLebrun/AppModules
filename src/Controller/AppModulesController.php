bec6b218716090e0ec05517aee34e4d482173591
Sophie Pan <pan.sophie.sco@gmail.com>
2021-01-21 01:25:58 +0100

    #2 Modifications Controller Maquette

--- src/Controller/AppModulesController.php
+++ src/Controller/AppModulesController.php
@@ -11,36 +11,59 @@ use Symfony\Component\Routing\Annotation\Route;
 class AppModulesController extends AbstractController
 {
     /**
-     * @Route("/", name="maquette")
+     * @Route("/", name="index")
      */
     public function index(): RedirectResponse
     {
-        return $this->redirectToRoute('/Semestre1');
+        return $this->redirectToRoute('semestre',['page'=>1],301);
     }
 
     /**
-     * @Route("/Semestre1", name="maquette")
+     * @Route("/semestre/{page<\d+>}", name="semestre")
      */
-    public function Semestre1(): Response
+    public function MaquetteEnseignement(int $page): Response
     {
         $semainerepo=$this->getDoctrine()->getRepository(Semaine::class);
 
-        $semainesUE11=$semainerepo->toutesLesSemainesDeLUE11();
-        $semainesUE12=$semainerepo->toutesLesSemainesDeLUE12();
+        $semainesUE1=""; $semainesUE2="";
 
-        $ddatedebscol = "2019-09-02";
-        $datedebscol = new \DateTime($ddatedebscol);
-        $semainedebscol = $datedebscol->format("W");
+        switch ($page){
+            case 1 :
+                $semainesUE1=$semainerepo->toutesLesSemainesDeLUE11();
+                $semainesUE2=$semainerepo->toutesLesSemainesDeLUE12();
+                break;
+            case 2 :
+                $semainesUE1=$semainerepo->toutesLesSemainesDeLUE21();
+                $semainesUE2=$semainerepo->toutesLesSemainesDeLUE22();
+                break;
+            case 3 :
+                $semainesUE1=$semainerepo->toutesLesSemainesDeLUE31();
+                $semainesUE2=$semainerepo->toutesLesSemainesDeLUE32();
+                break;
+            case 4 :
+                $semainesUE1=$semainerepo->toutesLesSemainesDeLUE41();
+                $semainesUE2=$semainerepo->toutesLesSemainesDeLUE42();
+                break;
+            case 5 :
+                $semainesUE1=$semainerepo->toutesLesSemainesDeLUE41();
+                $semainesUE2=$semainerepo->toutesLesSemainesDeLUE42();
+                break;
+        }
 
-        $ddatefinscol = "2020-01-26";
-        $datefinscol = new \DateTime($ddatefinscol);
-        $semainefinscol = $datefinscol->format("W");
+        $ddatedebs1 = "2019-09-02";
+        $datedebs1 = new \DateTime($ddatedebs1);
+        $semainedebs1 = $datedebs1->format("W");
+
+        $ddatefins1 = "2020-01-26";
+        $datefins1 = new \DateTime($ddatefins1);
+        $semainefins1 = $datefins1->format("W");
 
         return $this->render('AppModules/MaquetteEnseignement.html.twig', [
-            'semainesUE11'=>$semainesUE11,
-            'semainesUE12'=>$semainesUE12,
-            'semainedebscol'=>$semainedebscol,
-            'semainefinscol'=>$semainefinscol,
+            'numSemestre'=>$page,
+            'semainesUE1'=>$semainesUE1,
+            'semainesUE2'=>$semainesUE2,
+            'semainedebs1'=>$semainedebs1,
+            'semainefins1'=>$semainefins1,
         ]);
     }
 }
