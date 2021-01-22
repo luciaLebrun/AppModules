8c8daa6604d0c7f3f77b02915ae2b592b97a31bd
lucia <lebrunlucia@gmail.com>
2021-01-22 23:33:28 +0100

    add : Commande csv pour l'import

--- src/Controller/EnseignantController.php
+++ src/Controller/EnseignantController.php
@@ -3,174 +3,39 @@
 
 namespace App\Controller;
 
-use App\Entity\Enseignant;
-use App\Entity\Module;
-use App\Repository\EnseignantRepository;
-use Doctrine\ORM\EntityManagerInterface;
-use phpDocumentor\Reflection\Types\Null_;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Exception;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Component\Validator\Constraints\Json;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
-
 /**
  * Class EnseignantController
  * @package App\Controller
- * @Route("/enseignants")
  */
-class EnseignantController
+class EnseignantController extends AbstractController
 {
     /**
-     * @Route(name="api_enseignants_collection_get", methods={"GET"})
-     * @param EnseignantRepository $enseignantRepository
-     * @param SerializerInterface $serializer
-     * @return JsonResponse
+     * @Route("/import_enseignants", name="import_enseignants")
+     * @param KernelInterface $kernel
+     * @return Response
+     * @throws Exception
      */
-    public function collection(EnseignantRepository $enseignantRepository, SerializerInterface $serializer): JsonResponse
+    public function import(KernelInterface $kernel): Response
     {
-        return new JsonResponse(
-            $serializer->serialize($enseignantRepository->findAll(), "json", ["groups" => "get"]),
-            JsonResponse::HTTP_OK,
-            [],
-            true
-        );
-    }
+        $application = new Application($kernel);
+        $application->setAutoExit(false);
 
-    /**
-     * @Route("/{id}", name="api_enseignants_item_get", methods={"GET"})
-     * @param Enseignant $enseignant
-     * @param SerializerInterface $serializer
-     * @return JsonResponse
-     */
-    public function item(Enseignant $enseignant, SerializerInterface $serializer): JsonResponse
-    {
-        return new JsonResponse(
-            $serializer->serialize($enseignant, "json", ["groups" => "get"]),
-            JsonResponse::HTTP_OK,
-            [],
-            true
-        );
-    }
+        $input = new ArrayInput(array('command' => 'csv:import'));
+        $output = new BufferedOutput();
+        $application->run($input, $output);
 
-    /**
-     * @Route(name="api_enseignants_collection_post", methods={"POST"})
-     * @param Enseignant $enseignant
-     * @param SerializerInterface $serializer
-     * @param EntityManagerInterface $entityManager
-     * @param UrlGeneratorInterface $urlGenerator
-     * @param ValidatorInterface $validator
-     * @return JsonResponse
-     */
-    public function post(
-        Enseignant $enseignant,
-        SerializerInterface $serializer,
-        EntityManagerInterface $entityManager,
-        UrlGeneratorInterface $urlGenerator,
-        ValidatorInterface $validator
-    ): JsonResponse {
-        //$enseignant->addModule($entityManager->getRepository(Module::class)->findOneBy([]));
 
-        $rowNo = 1;
-        $response = new JsonResponse();
+        var_dump($output->fetch());
 
-        if(($file = fopen("./docs/CSV/Enseignant.csv", "r")) !== false)
-        {
-            while (($row = fgetcsv($file,1000,";")) !=false)
-            {
-                $num = count($row); // On compte le nb de colonnes
-                $rowNo++; // Nb ligne
-
-                for ($i=0; $i < $num; $i++) // Parcours des colonnes
-                {
-                    switch ($i) {
-                        case 1 :
-                            $response->setData(['trigramme' => $row[$i]]);
-                            //$enseignant->setTrigramme($row[$i]);
-                            break;
-                        case 2 :
-                            $enseignant->setPrenom($row[$i]);
-                            $enseignant->setNom($row[$i]);
-                            break;
-                        case 3 :
-                            $response->setData(['serviceDu' => $row[$i]]);
-                            //$enseignant->setServiceDu($row[$i]);
-                            break;
-                        case 4 :
-                            $enseignant->setStatut($row[$i]);
-                            break;
-                        case 5 :
-                            if($row[$i] != NULL)
-                            {
-                                $enseignant->setContact($row[$i]);
-                            }
-                            break;
-                    }
-                }
-            }
-            fclose($file);
-        }
-
-        $errors = $validator->validate($enseignant);
-
-        if ($errors->count() > 0)
-        {
-            return new JsonResponse($serializer->serialize($errors, 'json'), JsonResponse::HTTP_BAD_REQUEST, [], true);
-        }
-
-        $entityManager->persist($enseignant);
-        $entityManager->flush();
-
-        return new JsonResponse(
-            $serializer->serialize($response, "json", ["groups" => "get"]),
-            JsonResponse::HTTP_CREATED,
-            ["Location" => $urlGenerator->generate("api_enseignants_item_get", ["id" => $enseignant->getId()])],
-            true
-        );
-    }
-
-    /**
-     * @Route("/{id}",name="api_enseignants_item_put", methods={"PUT"})
-     * @param Enseignant $enseignant
-     * @param SerializerInterface $serializer
-     * @param EntityManagerInterface $entityManager
-     * @param ValidatorInterface $validator
-     * @return JsonResponse
-     */
-    public function put(
-        Enseignant $enseignant,
-        SerializerInterface $serializer,
-        EntityManagerInterface $entityManager,
-        ValidatorInterface $validator
-    ): JsonResponse {
-        $errors = $validator->validate($enseignant);
-
-        if ($errors->count() > 0) {
-            return new JsonResponse($serializer->serialize($errors, 'json'), JsonResponse::HTTP_BAD_REQUEST, [], true);
-        }
-
-        $entityManager->flush();
-
-        return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
-
-    }
-
-    /**
-     * @Route("/{id}", name="api_enseignants_item_delete", methods={"DELETE"})
-     * @param Enseignant $enseignant
-     * @param EntityManagerInterface $entityManager
-     * @return JsonResponse
-     */
-    public function delete(
-        Enseignant $enseignant,
-        EntityManagerInterface $entityManager
-    ): JsonResponse {
-
-        $entityManager->remove($enseignant);
-        $entityManager->flush();
-
-        return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
+        return $this->redirectToRoute('index');
     }
 
 }
