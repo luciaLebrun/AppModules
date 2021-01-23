8e0c22d26a635accfc5d20b1ffc27f116190154b
Sophie Pan <pan.sophie.sco@gmail.com>
2021-01-23 02:18:55 +0100

    #1 Refactoring : correction découplage métier + noms des variables en anglais

--- src/Domain/MaquetteEnseignement.php
+++ src/Domain/MaquetteEnseignement.php
@@ -6,14 +6,5 @@ namespace App\Domain;
 
 interface MaquetteEnseignement
 {
-    public function toutesLesSemaines():iterable;
-    public function toutesLesSemainesDeLUE11():iterable;
-    public function toutesLesSemainesDeLUE12():iterable;
-    public function toutesLesSemainesDeLUE21():iterable;
-    public function toutesLesSemainesDeLUE22():iterable;
-    public function toutesLesSemainesDeLUE31():iterable;
-    public function toutesLesSemainesDeLUE32():iterable;
-    public function toutesLesSemainesDeLUE41():iterable;
-    public function toutesLesSemainesDeLUE42():iterable;
-    public function toutesLesSemainesDeLP():iterable;
+    public function findEachWeekOfASemester($semester):iterable;
 }
--- src/Repository/SemaineRepository.php
+++ src/Repository/SemaineRepository.php
@@ -21,140 +21,35 @@ class SemaineRepository extends ServiceEntityRepository implements MaquetteEnsei
         parent::__construct($registry, Semaine::class);
     }
 
-    public function toutesLesSemaines(): iterable
+    public function findEachWeekOfASemester($semester): iterable
     {
-        // TODO: Implement toutesLesSemaines() method.
+        $sem = array();
+        for($i = 1; $i <= 2; $i++){
+            $UE = "M".$semester.$i."%";
+            $query=$this->_em->createQuery('SELECT E.trigramme AS responsable, M.intitule as module, S.semaine as semaine, NULLIF(S.CM,0) as CM, NULLIF(S.TD,0) as TD, NULLIF(S.TP,0) as TP
+                                        FROM App\Entity\Semaine S, App\Entity\Module M, App\Entity\Enseignant E
+                                        WHERE S.module = M.id
+                                            AND M.responsable = E.id
+                                            AND M.PPN LIKE :UE
+                                        ORDER BY M.intitule')
+                ->setParameter("UE", $UE);
+            $result = $query->getResult();
+            $sem['UE'.$i]=$result;
+        }
+        return $sem;
     }
 
-    public function toutesLesSemainesDeLUE11(): iterable
+    public function findEachWeekOfUE2OfASemester($semester): iterable
     {
+        $UE = "M".$semester."2%";
         //création de query avec DQL
         $query=$this->_em->createQuery('SELECT E.trigramme AS responsable, M.intitule as module, S.semaine as semaine, NULLIF(S.CM,0) as CM, NULLIF(S.TD,0) as TD, NULLIF(S.TP,0) as TP
                                         FROM App\Entity\Semaine S, App\Entity\Module M, App\Entity\Enseignant E
                                         WHERE S.module = M.id
                                             AND M.responsable = E.id
-                                            AND M.PPN LIKE \'M11%\'
-                                        ORDER BY M.intitule');
-        //récupération du résultat à partir de la query
-        $result = $query->getResult();
-        //on retourne le résultat
-        return $result;
-    }
-
-    public function toutesLesSemainesDeLUE12(): iterable
-    {
-        //création de query avec DQL
-        $query=$this->_em->createQuery('SELECT E.trigramme AS responsable, M.intitule as module, S.semaine as semaine, NULLIF(S.CM,0) as CM, NULLIF(S.TD,0) as TD, NULLIF(S.TP,0) as TP
-                                        FROM App\Entity\Semaine S, App\Entity\Module M, App\Entity\Enseignant E
-                                        WHERE S.module = M.id
-                                            AND M.responsable = E.id
-                                            AND M.PPN LIKE \'M12%\'
-                                        ORDER BY M.intitule');
-        //récupération du résultat à partir de la query
-        $result = $query->getResult();
-        //on retourne le résultat
-        return $result;
-    }
-
-    public function toutesLesSemainesDeLUE21(): iterable
-    {
-        //création de query avec DQL
-        $query=$this->_em->createQuery('SELECT E.trigramme AS responsable, M.intitule as module, S.semaine as semaine, NULLIF(S.CM,0) as CM, NULLIF(S.TD,0) as TD, NULLIF(S.TP,0) as TP
-                                        FROM App\Entity\Semaine S, App\Entity\Module M, App\Entity\Enseignant E
-                                        WHERE S.module = M.id
-                                            AND M.responsable = E.id
-                                            AND M.PPN LIKE \'M21%\'
-                                        ORDER BY M.intitule');
-        //récupération du résultat à partir de la query
-        $result = $query->getResult();
-        //on retourne le résultat
-        return $result;
-    }
-
-    public function toutesLesSemainesDeLUE22(): iterable
-    {
-        //création de query avec DQL
-        $query=$this->_em->createQuery('SELECT E.trigramme AS responsable, M.intitule as module, S.semaine as semaine, NULLIF(S.CM,0) as CM, NULLIF(S.TD,0) as TD, NULLIF(S.TP,0) as TP
-                                        FROM App\Entity\Semaine S, App\Entity\Module M, App\Entity\Enseignant E
-                                        WHERE S.module = M.id
-                                            AND M.responsable = E.id
-                                            AND M.PPN LIKE \'M22%\'
-                                        ORDER BY M.intitule');
-        //récupération du résultat à partir de la query
-        $result = $query->getResult();
-        //on retourne le résultat
-        return $result;
-    }
-
-    public function toutesLesSemainesDeLUE31(): iterable
-    {
-        //création de query avec DQL
-        $query=$this->_em->createQuery('SELECT E.trigramme AS responsable, M.intitule as module, S.semaine as semaine, NULLIF(S.CM,0) as CM, NULLIF(S.TD,0) as TD, NULLIF(S.TP,0) as TP
-                                        FROM App\Entity\Semaine S, App\Entity\Module M, App\Entity\Enseignant E
-                                        WHERE S.module = M.id
-                                            AND M.responsable = E.id
-                                            AND M.PPN LIKE \'M31%\'
-                                        ORDER BY M.intitule');
-        //récupération du résultat à partir de la query
-        $result = $query->getResult();
-        //on retourne le résultat
-        return $result;
-    }
-
-    public function toutesLesSemainesDeLUE32(): iterable
-    {
-        //création de query avec DQL
-        $query=$this->_em->createQuery('SELECT E.trigramme AS responsable, M.intitule as module, S.semaine as semaine, NULLIF(S.CM,0) as CM, NULLIF(S.TD,0) as TD, NULLIF(S.TP,0) as TP
-                                        FROM App\Entity\Semaine S, App\Entity\Module M, App\Entity\Enseignant E
-                                        WHERE S.module = M.id
-                                            AND M.responsable = E.id
-                                            AND M.PPN LIKE \'M32%\'
-                                        ORDER BY M.intitule');
-        //récupération du résultat à partir de la query
-        $result = $query->getResult();
-        //on retourne le résultat
-        return $result;
-    }
-
-    public function toutesLesSemainesDeLUE41(): iterable
-    {
-        //création de query avec DQL
-        $query=$this->_em->createQuery('SELECT E.trigramme AS responsable, M.intitule as module, S.semaine as semaine, NULLIF(S.CM,0) as CM, NULLIF(S.TD,0) as TD, NULLIF(S.TP,0) as TP
-                                        FROM App\Entity\Semaine S, App\Entity\Module M, App\Entity\Enseignant E
-                                        WHERE S.module = M.id
-                                            AND M.responsable = E.id
-                                            AND M.PPN LIKE \'M41%\'
-                                        ORDER BY M.intitule');
-        //récupération du résultat à partir de la query
-        $result = $query->getResult();
-        //on retourne le résultat
-        return $result;
-    }
-
-    public function toutesLesSemainesDeLUE42(): iterable
-    {
-        //création de query avec DQL
-        $query=$this->_em->createQuery('SELECT E.trigramme AS responsable, M.intitule as module, S.semaine as semaine, NULLIF(S.CM,0) as CM, NULLIF(S.TD,0) as TD, NULLIF(S.TP,0) as TP
-                                        FROM App\Entity\Semaine S, App\Entity\Module M, App\Entity\Enseignant E
-                                        WHERE S.module = M.id
-                                            AND M.responsable = E.id
-                                            AND M.PPN LIKE \'M42%\'
-                                        ORDER BY M.intitule');
-        //récupération du résultat à partir de la query
-        $result = $query->getResult();
-        //on retourne le résultat
-        return $result;
-    }
-
-    public function toutesLesSemainesDeLP(): iterable
-    {
-        //création de query avec DQL
-        $query=$this->_em->createQuery('SELECT E.trigramme AS responsable, M.intitule as module, S.semaine as semaine, NULLIF(S.CM,0) as CM, NULLIF(S.TD,0) as TD, NULLIF(S.TP,0) as TP
-                                        FROM App\Entity\Semaine S, App\Entity\Module M, App\Entity\Enseignant E
-                                        WHERE S.module = M.id
-                                            AND M.responsable = E.id
-                                            AND M.PPN LIKE \'M5%\'
-                                        ORDER BY M.intitule');
+                                            AND M.PPN LIKE :UE
+                                        ORDER BY M.intitule')
+            ->setParameter("UE", $UE);
         //récupération du résultat à partir de la query
         $result = $query->getResult();
         //on retourne le résultat
