6f99155d97c935db2b8958e11da9b4dad8f7c822
Sophie Pan <sophie.pan@etudiant.univ-lr.fr>
2021-01-28 21:03:18 +0100

    test semainerepository

--- tests/Repository/SemaineRepositoryTest.php
+++ tests/Repository/SemaineRepositoryTest.php
@@ -24,7 +24,22 @@ class SemaineRepositoryTest extends KernelTestCase
     public function testFindEachWeekOfASemester(){
         $semester = $this->entityManager->getRepository(Semaine::class)->findEachWeekOfASemester(2);
 
+        // Test 1 : the right number of elements are returned
         $this->assertCount(2, $semester);
+        $this->assertCount(58, $semester['UE1']);
+        $this->assertCount(6, $semester['UE1'][1]);
+
+        // Test 2 : that it returns the correct form of object
+        $this->assertIsIterable($semester);
+        $this->assertIsIterable($semester['UE1']);
+        $this->assertIsIterable($semester['UE2'][1]);
+        $this->assertIsFloat($semester['UE2'][1]['semaine']);
+        $this->assertIsString($semester['UE2'][1]['responsables']);
+
+        // Test 3 : that they’re in the right order
+        $this->assertStringContainsString('BCOO2',$semester['UE1'][0]['module']);
+        $this->assertStringContainsString('RES2',$semester['UE1'][40]['module']);
+        $this->assertStringContainsString('SI2',$semester['UE1'][57]['module']);
     }
 
     protected function tearDown(): void
