3b105dc0e85a158cbc9442837ab50d737fda3590
Sophie Pan <sophie.pan@etudiant.univ-lr.fr>
2021-01-28 16:56:13 +0100

    ajout répertoire test

---
+++ tests/Repository/SemaineRepositoryTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class SemaineRepositoryTest extends TestCase
+{
+    public function testSomething()
+    {
+        $this->assertTrue(true);
+    }
+}
